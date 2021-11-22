e89d3c48546109e90c9fe3dacd1b25ecded33ec2
gromi1985 <alpauta73@gmail.com>
2021-11-22 19:27:04 +0100

    Shop Level 1

--- js/checkout.js
+++ js/checkout.js
@@ -1,3 +1,27 @@
+
+const formulario = document.getElementsByClassName("form");
+const inputsField = document.querySelectorAll(".form div input");
+
+
+const expresiones = {
+    firstName: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letrasy espacios, pueden llevar acentos
+    lastName: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
+    password: /(?=\w*\d)(?=\w*[A-Za-z])\S{3,12}$/, // 3 a 12 digitos.
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    phoneNo: /^\d{3,14}$/, // 3 a 14 numeros.
+    address: /^[#.0-9a-zA-Z\s,-]+$/ //simple address
+}
+
+const campos = {
+    firstName: false,
+    lastName: false,
+    password: false,
+    email: false,
+    phoneNo: false
+}
+
+
+
 // Get the input fields
 var password = document.querySelector(".password");
 var phone = document.querySelector('.phone');
@@ -5,10 +29,73 @@ var name = document.querySelector('.name');
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+var errorName = document.getElementById('errorName');
+var errorPhone = document.getElementById('errorPhone');
 
 // Exercise 8
+
+// Validate fields entered by the user: name, phone, password, and email
+//Valido los campos a medida que se van rellenando
+const validarFormulario = (e) => {
+    switch (e.name) {
+        case 'firstName':
+            validarCampo(expresiones.firstName, e, 'firstName');
+            break;
+        case 'lastName':
+            validarCampo(expresiones.lastName, e, 'lastName');
+            break;
+        case "password":
+            validarCampo(expresiones.password, e, 'password');
+            break;
+        case "email":
+            validarCampo(expresiones.email, e, 'email');
+            break;
+        case "phoneNo":
+            validarCampo(expresiones.phoneNo, e, 'phoneNo');
+            break;
+    }
+}
+
+const validarCampo = (expresion, input,campo) => {
+    if (expresion.test(input.value) ) {
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+    } else {
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+    }
+}
+
+
+//Valido los campos que no != vacio cuando se realiza el submit
+const validarEmptyFields = (input) => {
+    if (input.value === '') {
+        document.getElementById(`grupo__${input.name}`).classList.add('formulario__grupo-incorrecto');
+        document.querySelector(`#grupo__${input.name} .formulario__input-error`).innerHTML='This field can not empty';
+        document.querySelector(`#grupo__${input.name} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        return false;
+    }
+    else 
+        return true;
+}
+
+const validarSizeFields = (input) => {
+    if (input.value.length < 3) {
+        document.getElementById(`grupo__${input.name}`).classList.add('formulario__grupo-incorrecto');
+        document.querySelector(`#grupo__${input.name} .formulario__input-error`).innerHTML='Minimum number of characters is 3';
+        document.querySelector(`#grupo__${input.name} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        return false;
+    }
+    else 
+        return true;
+}
+
 function validate() {
-    // Validate fields entered by the user: name, phone, password, and email
+    inputsField.forEach((input) => {
+        if(validarEmptyFields(input))
+         if (validarSizeFields(input))
+             validarFormulario(input);
+    });
+
+return false;
 }
--- js/shop.js
+++ js/shop.js
@@ -80,42 +80,115 @@ var total = 0;
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
+    let i=0;
+    while(products[i].id !== id) ++i;
+
     // 2. Add found product to the cartList array
+    cartList.push(products[i]);
+
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cartList.splice(0,cartList.length);
 }
 
 // Exercise 3
-function calculateSubtotals() {
+function calculateSubtotals_v1() {
     // 1. Create a for loop on the "cartList" array 
+    for( let i in cartList){
+        subtotal[cartList[i].type].value += cartList[i].price;
+    }
     // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
 }
 
 // Exercise 4
 function calculateTotal() {
     // Calculate total price of the cart either using the "cartList" array
+    for(let key in subtotal)
+    {
+        total += subtotal[key].value;
+    }
+
+    applyPromotionsCart();
 }
 
 // Exercise 5
+
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
+    for(let elementCart in cartList)
+    {
+        const objectReferenced = cart.find((element) => element.id === cartList[elementCart].id);
+        if  (objectReferenced  == undefined) {
+            let newElement = cartList[elementCart];
+            newElement["quantity"] = 1;
+            newElement["subtotal"]  = cartList[elementCart].price;
+            newElement["subtotalWithDiscount"] = 0; 
+            cart.push(newElement);
+        }
+        else{
+        objectReferenced.quantity++;
+        objectReferenced.subtotal += cartList[elementCart].price; 
+        }
+
+    }
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+
 }
 
 // Exercise 6
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    for(let elementCart in cart)
+    {
+        if(cart[elementCart].quantity >= 3 && cart[elementCart].type === "grocery" && cart[elementCart].name==="cooking oil")
+        {
+            cart[elementCart].subtotalWithDiscount = cart[elementCart].quantity *10;
+        }
+        if(cart[elementCart].quantity >= 10 && cart[elementCart].type === "grocery" && cart[elementCart].name==="Instant cupcake mixture" )
+        {
+            cart[elementCart].subtotalWithDiscount = cart[elementCart].quantity *(2/3) * cart[elementCart].price;
+        }
+    }
+
 }
 
 // Exercise 7
 function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
+    for(itemProduct in products){
+        if(products[itemProduct].id === id)
+        {
+            const objectReferenced= cart.find((element) => element.id === id);
+            if  (objectReferenced  == undefined) {
+                let newElement = products[itemProduct];
+                newElement["quantity"] = 1;
+                newElement["subtotal"]  = products[itemProduct].price;
+                newElement["subtotalWithDiscount"] = 0; 
+                cart.push(newElement);
+            }
+            else{
+            objectReferenced.quantity++;
+            objectReferenced.subtotal += products[itemProduct].price; 
+            }
+        }
+    }
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    calculateSubtotals();
 }
+
+function calculateSubtotals() {
+    // 1. Create a for loop on the "cart" array 
+    for(let itemCart in cart){
+        subtotal[cart[itemCart].type].value +=cart[itemCart].quantity * cart[itemCart].price;
+    }
+    // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+    calculateTotal();
+}
+
+
 
 // Exercise 9
 function removeFromCart(id) {
